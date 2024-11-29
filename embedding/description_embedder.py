4473557e385fdb34e969ce61e67e85c861ff06d5
V8-Luky <45991846+V8-Luky@users.noreply.github.com>
2024-11-29 13:44:30 +0100

    Adapt database structure to new, cleaned data and repopulate database (#52)

--- embedding/description_embedder.py
+++ embedding/description_embedder.py
@@ -6,4 +6,4 @@ from embedding.game_embedder import GameEmbedder
 
 class DescriptionEmbedder(GameEmbedder):
     def create_game_embeddings(self, games: DataFrame) -> list[ndarray]:
-        return self.create_embeddings(games['description'].tolist())
+        return self.create_embeddings(games['About the game'].tolist())
