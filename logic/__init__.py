c68a268765802b6506ea2ac12c0b57de9c52dcc9
Luca Kyburz <kyburzluca@gmail.com>
2024-12-16 13:40:42 +0100

    Cleanup logic folder

---
+++ logic/__init__.py
@@ -0,0 +1,6 @@
+"""Contains game specific logic and classes used by the backend."""
+
+from .daily_target_game import DailyTargetGame
+from .game import Game
+from .game_comparison import GameComparison
+from .hint_generator import HintGenerator
--- logic/hint_generator.py
+++ logic/hint_generator.py
@@ -1,3 +1,5 @@
+"""Responsible for generating hints for the player to guess the targeted game."""
+
 from genai.gen_ai_model import GenAIModel
 
 DEFAULT_HINT_PROMPT = """
@@ -12,11 +14,29 @@ You must not make it too easy.
 
 
 class HintGenerator:
+    """
+    Responsible for generating hints for the player to guess the targeted game.
+
+    Attributes:
+        model: The model to use for generating hints.
+        hint_prompt: The prompt to use for generating hints.
+    """
+
     def __init__(self, api_key: str, hint_prompt: str = DEFAULT_HINT_PROMPT):
         self._model = GenAIModel(api_key=api_key)
         self._hint_prompt = hint_prompt
 
     def generate_hint(self, target_game_name: str, guessed_game_name: str) -> str:
+        """
+        Generates a hint based on the names of the target game and the guessed game.
+
+        Args:
+            target_game_name (str): The name of the target game.
+            guessed_game_name (str): The name of the game guessed by the player.
+
+        Returns:
+            str: The generated hint or None if the model fails to generate a hint.
+        """
         try:
             prompt = self._hint_prompt.format(
                 target_game_name=target_game_name, guessed_game_name=guessed_game_name
